0ccf23b47d28d1046ad1cf6546167f07bf028975
Tuyen Dang <jesminedang@gmail.com>
2020-03-16 21:27:58 +0100

    first commit

---
+++ src/components/Titles.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Titles = () => (
+
+    <div>
+        <h1>      Weather View     </h1>
+        <h3>    What's weather today?   </h3>
+    </div>
+
+)
+
+export default Titles;
